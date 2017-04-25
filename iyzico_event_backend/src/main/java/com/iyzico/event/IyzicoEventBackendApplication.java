806eaba30d470e0a1d6520ff6b2f46bafd9a49ec
TCMBAS <mahmut.bas@turkcell.com.tr>
2017-04-25 23:06:00 +0300

    iyzico event project initial commit

---
+++ iyzico_event_backend/src/main/java/com/iyzico/event/IyzicoEventBackendApplication.java
@@ -0,0 +1,12 @@
+package com.iyzico.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IyzicoEventBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IyzicoEventBackendApplication.class, args);
+	}
+}

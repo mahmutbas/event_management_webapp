245f60a2149115d658021519c0f8d293bb33652e
TCMBAS <mahmut.bas@turkcell.com.tr>
2017-04-26 23:30:03 +0300

    TDD started with model classes
    (cherry picked from commit c31f24f)

---
+++ iyzico_event/src/main/java/com/iyzico/event/IyzicoEventApplication.java
@@ -0,0 +1,12 @@
+package com.iyzico.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IyzicoEventApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(IyzicoEventApplication.class, args);
+	}
+}
---
+++ iyzico_event/src/main/java/com/iyzico/event/util/UpperCaseUUIDGenerator.java
@@ -0,0 +1,31 @@
+package com.iyzico.event.util;
+
+import com.iyzico.event.model.AbstractEntity;
+
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.id.UUIDGenerator;
+
+import java.io.Serializable;
+
+/**
+ * Created by TCMBAS on 25/04/2017.
+ */
+public class UpperCaseUUIDGenerator extends UUIDGenerator
+{
+    @Override
+    public Serializable generate(SessionImplementor session, Object object) throws HibernateException
+    {
+        if (object instanceof AbstractEntity)
+        {
+            AbstractEntity abstractEntity = (AbstractEntity) object;
+            if (abstractEntity.getId() != null)
+            {
+                return abstractEntity.getId();
+            }
+        }
+        Serializable generated = super.generate(session, object);
+        return StringUtils.upperCase((String) generated);
+    }
+}

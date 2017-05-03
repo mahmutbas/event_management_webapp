29041bd182af4d00ba202e27cdddd43a0ca8b5ba
TCMBAS <mahmut.bas@turkcell.com.tr>
2017-05-04 02:56:10 +0300

    eventPage date format correction
    (cherry picked from commit ba7a266)

--- src/main/java/com/iyzico/event/controller/event/EventController.java
+++ src/main/java/com/iyzico/event/controller/event/EventController.java
@@ -12,9 +12,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
 import java.io.IOException;
+import java.time.ZoneId;
+import java.util.Date;
 
 /**
  * Created by TCMBAS on 02/05/2017.
@@ -37,9 +37,15 @@ public class EventController extends BaseController
     @Getter
     private String eventName;
 
+    @Setter
+    @Getter
+    private Date eventDate;
+
     public void init() throws IOException
     {
         event = eventService.findByEventName(StringUtils.isEmpty(this.eventName) ? DEFAULT_EVENT_NAME : this.eventName);
+        //todo type="localdate" will be in xhtml page with jsf 2.3
+        eventDate = Date.from(event.getEventDate().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
         eventName = null;
         if (event == null)
         {
